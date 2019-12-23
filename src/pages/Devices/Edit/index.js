6529bbca31adfab5ae2f2ac650af949059d0580c
Jonas <jonasvargaski@hotmail.com>
2019-12-23 12:42:35 -0300

    change title of page

--- src/pages/Devices/Edit/index.js
+++ src/pages/Devices/Edit/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Background from '~/components/Background';
 import { Container } from './styles';
 
 export default function Edit() {
-  return <Container />;
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
 }
+
+Edit.navigationOptions = ({ navigation }) => ({
+  title: 'Visualizar Controlador',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
