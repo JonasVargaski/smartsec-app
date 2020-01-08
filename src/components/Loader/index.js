a83f9aa1d91bf43d3524f26b5a5d823d3667b520
Jonas Vargaski <jonasvargaski@hotmail.com>
2020-01-07 23:25:08 -0300

    change styles off loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 
-export default function Loader({ ...rest }) {
+export default function Loader({ style, ...rest }) {
   return (
-    <Container>
+    <Container style={style}>
       <ActivityIndicator {...rest} color="#7560ec" />
     </Container>
   );
 }
+
+Loader.propTypes = {
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+Loader.defaultProps = {
+  style: {},
+};
--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1;
+  position: absolute;
   align-items: center;
+  justify-content: center;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 `;
